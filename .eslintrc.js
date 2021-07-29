a79d8f5c50aa025049c13acb3da83c78b081047f
Markus Dobmann <m.do@posteo.net>
2021-07-29 15:03:34 +0200

    chore: migrate `apps/wpcom-block-editor` to `import/order`

--- .eslintrc.js
+++ .eslintrc.js
@@ -179,6 +179,7 @@ module.exports = {
 			files: [
 				'apps/editing-toolkit/**/*',
 				'apps/notifications/**/*',
+				'apps/wpcom-block-editor/**/*',
 				'client/auth/**/*',
 				'client/boot/**/*',
 				'client/controller/**/*',
