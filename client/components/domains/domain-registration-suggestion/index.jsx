6dcc0b8fa453554982eb2b76e60ec192930066de
Leonardo Sameshima Taba <leonardo.taba@automattic.com>
2021-07-30 11:18:21 -0300

    Show checkmark items only in domains page and not in signup

--- client/components/domains/domain-registration-suggestion/index.jsx
+++ client/components/domains/domain-registration-suggestion/index.jsx
@@ -363,7 +363,7 @@ class DomainRegistrationSuggestion extends React.Component {
 	}
 
 	renderMatchReason() {
-		if ( this.props.showStrikedOutPrice ) {
+		if ( this.props.isReskinned ) {
 			return null;
 		}
 
